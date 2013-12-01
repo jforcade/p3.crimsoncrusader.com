98913eb997305d9974115eb26deec907388fbf98
Jose Forcade <jforcade@g.harvard.edu>
2013-11-30 23:56:41 -0500

    added pages, js code to distinguish question types, etc

--- js/ApChem1.js
+++ js/ApChem1.js
@@ -11,12 +11,42 @@ $('input[name=answer]').click(function() {
 	var label = $(this).prev();
 	uranswer = label.html();
 	
-	console.log("You clicked on " + answer);
-	alert("You selected " + answer);
+	//console.log("You clicked on " + uranswer);
+	//alert("You selected " + uranswer);
 	
 });
 
 $('input[id=check-btn]').click(function() {
+
+	console.log("the answertype html = " + $('#answertype').html());
+	var type=$('#answertype').html();
+	console.log("the var type = " + type);
+
+	switch(type)	
+	{
+	case 1:
+		console.log("case 1 called");
+		singleAnswer();
+		break;
+		
+	case 2:
+		checkAnswer();
+		break;
+		
+	case 3:
+		checkAnswers();
+		break;
+		
+	default:
+		singleAnswer();
+		break;
+	}
+		
+});
+
+function singleAnswer() {
+
+	console.log("singleAnswer was called");
 
 	if (uranswer==$('#theanswer').html()) {
 	
@@ -31,16 +61,8 @@ $('input[id=check-btn]').click(function() {
 		} else {
 			
 			$('#update').html("Try again! ");
-		
-		}
+		}	
 	}
 	
-	/*
-	 * You have to check if an answer has been selected and display "please choose an answer" 
-	 * I have to check the choice against the right answer.
-	 * 
-	 console.log("You clicked the next button");
-	$('#qhh').html = "Question 2";*/
- 
-});
+}
 
